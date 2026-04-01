d0f280db5da78e259b5b24420e2ced7c0b745367
uswii <uswa@raresense.so>
2026-04-01 00:43:35 +0000

    fix: rename I Acknowledge button to I Agree

--- src/pages/OnboardingWelcome.tsx
+++ src/pages/OnboardingWelcome.tsx
@@ -398,7 +398,7 @@ export default function OnboardingWelcome() {
             onClick={handleAcknowledge}
           >
             {submitting && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
-            I Acknowledge
+            I Agree
           </Button>
         </div>
 
